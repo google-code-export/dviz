4a40b7ee1169cd9b512b3e5a15913d27bc4b24da
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2009-09-16 19:46:21 +0000

    Minimized main to a single test case for the 'spacing bug'

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,16 +1,12 @@
-#include "MainWindow.h"
-#include "RenderOpts.h"
-
 #include <QStyle>
 #include <QSettings>
 
-#if defined(STATIC_LINK)
-Q_IMPORT_PLUGIN(qgif)
-Q_IMPORT_PLUGIN(qjpeg)
-Q_IMPORT_PLUGIN(qsvg)
-Q_IMPORT_PLUGIN(qtiff)
-#endif
+#include <QtGui/QApplication>
+#include <QFont>
+#include <QFontMetrics>
 
+// RenderOpts stuff is only here to satisfy the linker, not used in this test
+#include "RenderOpts.h"
 // init RenderOpts defaults
 bool RenderOpts::LastMirrorEnabled = true;
 bool RenderOpts::ARGBWindow = false;
@@ -20,108 +16,18 @@ bool RenderOpts::OxygenStyleQuirks = false;
 bool RenderOpts::DisableVideoProvider = false;
 QColor RenderOpts::hiColor;
 
-#include <assert.h>
-
-#include <QtGui/QApplication>
-
-#include "XmlRead.h"
-#include "XmlSave.h"
-#include "model/ItemFactory.h"
-#include "model/Slide.h"
-#include "model/TextItem.h"
 
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
 		
-	#if !defined(Q_OS_MAC) // raster on OSX == b0rken
-		// use the Raster GraphicsSystem as default on 4.5+
-		#if QT_VERSION >= 0x040500
-		QApplication::setGraphicsSystem("raster");
-		#endif
-	#endif
-
-	app.setApplicationName("DViz");
-	app.setApplicationVersion("0.1.0");
-	app.setOrganizationName("Josiah Bryan");
-	app.setOrganizationDomain("mybryanlife.com");
-
-	RenderOpts::OxygenStyleQuirks = app.style()->objectName() == QLatin1String("oxygen");
+	QFont font;
+	QFont("Sans-Serif",12);
+	QFontMetrics metrics(font);
+	QRect textRect1 = metrics.boundingRect("test");
+	QRect textRect2 = metrics.boundingRect("test ");
+	qDebug("Without Space: %d, With Space: %d", textRect1.width(), textRect2.width());
 	
-	QSettings s;
-	//RenderOpts::FirstRun = s.value("fotowall/firstTime", true).toBool();
-	RenderOpts::hiColor = app.palette().color(QPalette::Highlight);
-	RenderOpts::DisableVideoProvider = app.arguments().contains("-novideo");
-	//s.setValue("fotowall/firstTime", false);
-
-	
-
-	MainWindow mw;
-	//mw.showMaximized();
-	mw.show();
-	
-	return app.exec();
-	
-// 	printf("\n\n");
-// 	
-// 	//printf("Thanks for the fish!\n");
-// 	if(QFile("test.xml").exists())
-// 	{
-// 		XmlRead r("test.xml");
-// 		Slide s;
-// 		r.readSlide(&s);
-// 		
-// 		QList<AbstractItem *> items = s.itemList();
-// 		AbstractItem * item = items.at(0);
-// 		
-// 		assert(item != NULL);
-// 		
-// 		printf("> Load Test:\n");
-// 		printf("Item Class: %d\n",item->itemClass());
-// 		printf("Item Name: %s\n",item->itemName().toAscii().constData());
-// 		printf("Item Id: %d\n",item->itemId());
-// 		
-// 		if(item->itemClass() == ITEM_TEXT )
-// 		{
-// 			printf("Text Item: Text: '%s'", ((TextItem *)item)->text().toAscii().constData());
-// 		}
-// 		else
-// 		{
-// 			printf("(Unknown item class)\n");
-// 		}
-// 		
-// 		
-// 	}
-// 	else
-// 	{
-// 		Slide s;
-// 		TextItem *t = s.createText(QPoint());
-// 		t->setText("Hello World!");
-// 		t->setPos(QPointF(10,10));
-// 		t->setItemName("TextItem-1");
-// 		t->setItemId(ItemFactory::nextId());
-// 		
-// 		printf("> Save Test:\n");
-// 		printf("Item Class: %d\n",t->itemClass());
-// 		printf("Item Name: %s\n",t->itemName().toAscii().constData());
-// 		printf("Item Id: %d\n",t->itemId());
-// 		
-// 		if(t->itemClass() == ITEM_TEXT )
-// 		{
-// 			printf("Text Item: Text: '%s'", t->text().toAscii().constData());
-// 		}
-// 		else
-// 		{
-// 			printf("(Unknown item class)\n");
-// 		}
-// 		
-// 		XmlSave save("test.xml");
-// 		save.saveSlide(&s);
-// 	}
-// 	
-// 	
-// 	printf("\n\n");
-// 	
 	return -1;
 }
 
