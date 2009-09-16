f799fe9a62cb8b6467fa51be6cea91ec7ac25cf5
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2009-09-16 14:04:38 +0000

    Forgot to add BoxContent/BoxItem to svn - oops.

---
+++ trunk/src/model/BoxItem.h
@@ -0,0 +1,28 @@
+#ifndef BOXITEM_H
+#define BOXITEM_H
+
+#include "AbstractVisualItem.h"
+
+
+class BoxItem : public AbstractVisualItem
+{
+	Q_OBJECT
+	
+public:
+	BoxItem();
+	~BoxItem();
+	
+	// ::AbstractVisualItem
+	AbstractContent * createDelegate(QGraphicsScene*);
+	
+	// ::AbstractItem
+	
+	// class identification
+	quint32 itemClass() const { return 0x0003; }
+	
+	// loading/saving
+        void toXml(QDomElement & parentElement) const;
+};
+
+
+#endif
