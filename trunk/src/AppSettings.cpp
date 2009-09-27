4db09d352b139328afc38a1973487c51d5854a13
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2009-09-27 01:08:33 +0000

    Adding AppSettings as a generic settings load/save location for app-wide settings.

---
+++ trunk/src/AppSettings.cpp
@@ -0,0 +1,88 @@
+#include "AppSettings.h"
+
+#include <QSettings>
+
+#include <QList>
+#include "model/Output.h"
+
+QList<Output*> AppSettings::m_outputs;
+
+void AppSettings::load()
+{
+	m_outputs.clear();
+
+	QSettings s;
+	loadOutputs(&s);
+
+}
+
+void AppSettings::save()
+{
+	QSettings s;
+	saveOutputs(&s);
+}
+
+
+void AppSettings::saveOutputs(QSettings *s)
+{
+	s->setValue("outputs/num",m_outputs.size());
+	int num = 0;
+	foreach(Output *out, m_outputs)
+	{
+		s->setValue(QString("outputs/output%1").arg(num++), out->toByteArray());
+	}
+}
+
+void AppSettings::loadOutputs(QSettings *s)
+{
+	int num = s->value("outputs/num").toInt();
+	for(int x=0; x<num;x++)
+	{
+		Output * out = new Output();
+		out->fromByteArray(s->value(QString("outputs/output%1").arg(x)).toByteArray());
+		m_outputs.append(out);
+	}
+
+	if(num == 0)
+	{
+		setupSystemPresetOutputs();
+	}
+}
+
+void AppSettings::setupSystemPresetOutputs()
+{
+	Output * out = new Output();
+		out->setIsSystem(true);
+		out->setIsEnabled(true);
+		out->setName("Live");
+		out->setOutputType(Output::Screen);
+		out->setScreenNum(0);
+	m_outputs << out;
+
+	out = new Output();
+		out->setIsSystem(true);
+		out->setIsEnabled(false);
+		out->setName("Foldback");
+		out->setOutputType(Output::Screen);
+		out->setScreenNum(1);
+	m_outputs << out;
+
+	out = new Output();
+		out->setIsSystem(true);
+		out->setIsEnabled(false);
+		out->setName("Secondary");
+		out->setOutputType(Output::Screen);
+		out->setScreenNum(2);
+	m_outputs << out;
+}
+
+void AppSettings::addOutput(Output *out) { m_outputs.append(out); }
+
+bool AppSettings::removeOutput(Output *out)
+{
+	if(out->isSystem())
+		return false;
+
+	m_outputs.removeAll(out);
+	return true;
+}
