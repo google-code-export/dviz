b04389a0bc7a2522b1e8d7585cc884ecffd801ec
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2009-10-05 01:07:57 +0000

    Updated layout of GenericItemConfig.ui's Background tab - moved radio buttons out of the frames inorder to use auto-exclusion.

--- trunk/src/items/VideoFileConfig.cpp
+++ trunk/src/items/VideoFileConfig.cpp
@@ -8,9 +8,13 @@ VideoFileConfig::VideoFileConfig(VideoFileContent * v, QGraphicsItem * parent)
     , m_vid(v)
 {
 	m_commonUi->bgOptGradient->setVisible(false);
+	m_commonUi->bgGradient->setVisible(false);
 	m_commonUi->bgOptColor->setVisible(false);
+	m_commonUi->bgColor->setVisible(false);
 	m_commonUi->bgOptImage->setVisible(false);
+	m_commonUi->bgImage->setVisible(false);
 	m_commonUi->bgOptNoBg->setVisible(false);
+	m_commonUi->bgNone->setVisible(false);
 	m_commonUi->bgVideo->setVisible(false);
 	
 	int idx = m_commonUi->tabWidget->indexOf(m_commonUi->backgroundTab);
