e91679c90107d6134a7d4be971733f342c538711
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2009-11-03 15:12:17 +0000

    Added actual import code to ImageImportDialog and added a few more options to the import dialog UI.

--- src/ImageImportDialog.h
+++ src/ImageImportDialog.h
@@ -7,17 +7,24 @@ namespace Ui {
     class ImageImportDialog;
 }
 
+class Document;
+class DocumentListModel;
 class ImageImportDialog : public QDialog {
     Q_OBJECT
 public:
-    ImageImportDialog(QWidget *parent = 0);
+    ImageImportDialog(Document *doc, QWidget *parent = 0);
     ~ImageImportDialog();
 
 protected:
     void changeEvent(QEvent *e);
+    void accept();
+    void importDirBrowse();
+    void copyToDirBrowse();
 
 private:
     Ui::ImageImportDialog *m_ui;
+    Document *m_doc;
+    DocumentListModel *m_model;
 };
 
 #endif // IMAGEIMPORTDIALOG_H
