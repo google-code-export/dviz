99e0e8391ae35ab4ec17652c21752f1d8b2a0aca
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2009-11-23 00:27:54 +0000

    Added Next/Prev buttons to ControlServer.cpp. Still need to get HTMl working right. Fixed BackgroundContent caching wrong file when same filename.

--- src/items/BackgroundContent.cpp
+++ src/items/BackgroundContent.cpp
@@ -362,7 +362,7 @@ void BackgroundContent::setImageFile(const QString &file)
 		QString cacheKey = QString("%1/%2/%3-%4x%5%6")
 					.arg(AppSettings::cachePath())
 					.arg(BG_IMG_CACHE_DIR)
-					.arg(MD5::md5sum(QFileInfo(file).fileName()))
+					.arg(MD5::md5sum(file))
 					.arg(size.width())
 					.arg(size.height())
 					.arg(sceneContextHint() == MyGraphicsScene::Preview ? "-icon192" : "");
