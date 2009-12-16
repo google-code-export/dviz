7388f47ed9dde18e2121802d45339d454ad3b833
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2009-12-16 06:07:11 +0000

    Added multithreaded loading of image thumbnails to src/imgtool/workspace

--- src/imgtool/workspace/DirectoryListModel.h
+++ src/imgtool/workspace/DirectoryListModel.h
@@ -8,6 +8,81 @@
 #include <QFileInfo>
 #include <QDir>
 #include <QHash>
+#include <QRunnable>
+#include <QMutex>
+
+class ThreadedIconProvider;
+class LoadImageTask : public QObject, public QRunnable
+{
+	Q_OBJECT
+public:
+	LoadImageTask(const QFileInfo &info, const QSize&, QList<QSize> standardSizesNeeded, ThreadedIconProvider*);
+	void run();
+	
+signals:
+	void imageLoaded(const QFileInfo&, const QSize&, const QImage&, bool explicitRequest);
+	void taskFinished(const QFileInfo&);
+	
+private:	
+	QFileInfo m_info;
+	QSize m_size;
+	QList<QSize> m_need; 
+	ThreadedIconProvider * m_provider;
+};
+
+class ThreadedIconProvider : public QObject
+{
+	Q_OBJECT
+public:
+	ThreadedIconProvider();
+	bool requestIcon(const QFileInfo&, const QSize&);
+	
+	int queueSize();
+	
+	void clearQueue();
+	
+	void prioritize(const QFileInfo&);
+	
+// 	typedef enum StandardSize
+// 	{
+// 		_160x120,
+// 		_320x240,
+// 		_640x480,
+// 		_1024x768,
+// 		_1600x1200
+// 	};
+
+signals:
+	void iconReady(const QFileInfo&, const QPixmap&);
+	
+	void queueSizeChanged(int);
+	void queueEmpty();
+	
+protected:
+	friend class LoadImageTask;
+	
+	bool isSizeCached(const QFileInfo&, const QSize&);
+	QList<QSize> standardSizes(); 
+	
+	QSize nextLargestStandardSize(const QSize&);
+	
+	QString cacheKey(const QFileInfo&, const QSize&);
+	QString cacheFile(const QString&);
+	
+private slots:
+	void imageLoaded(const QFileInfo&, const QSize&, const QImage&, bool explicitRequest);
+	void taskFinished(const QFileInfo&);
+	
+	void processFirstItem();
+	
+private:
+	int m_queueSize;
+	QMutex m_queueSizeMutex;
+	QList<QSize> m_standardSizes;
+	QList<QString> m_inProcess;
+	QHash<QString,QSize> m_sizesInProcess;
+	
+};
 
 class DirectoryListModel : public QAbstractListModel
 {
@@ -54,9 +129,14 @@ public:
 	void setNameFilters(const QStringList &list) { setFilters(list); } 
 	
 	
+public slots:
+	void prioritize(const QFileInfo&);
+	void prioritize(const QModelIndex& idx) { prioritize(fileInfo(idx)); }
+	
 	
 private slots:
 	void makePixmaps();
+	void iconReady(const QFileInfo&, const QPixmap&);
 	
 	// for QFileSystemWatcher
 // 	void directoryChanged ( const QString & path );
@@ -96,6 +176,8 @@ protected:
 	bool m_lockRowcount;
 	
 	int m_nameLengthMax;
+	
+	ThreadedIconProvider * m_threadedIconProvider;
 	
 	
 // 	QFileSystemWatcher * m_watcher;
