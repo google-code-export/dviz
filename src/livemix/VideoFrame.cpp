c03ee7f723d419168f5c1bff2981c83e8fc5224a
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2010-07-21 12:27:31 +0000

    Added VideoFrame.* missing from r556

---
+++ src/livemix/VideoFrame.cpp
@@ -0,0 +1,49 @@
+#include "VideoFrame.h"
+/*
+VideoFrame::VideoFrame()
+{
+	d = new VideoFramePrivateData;
+}
+
+VideoFrame::VideoFrame(const QImage &frame, int holdTime)
+{
+	d = new VideoFramePrivateData;
+	setHoldTime(holdTime);
+	setImage(frame);
+}
+
+VideoFrame::VideoFrame(const VideoFrame& other)
+	: d(other.d)
+{
+}
+
+bool VideoFrame::isEmpty()
+{
+	return d->time < 0 || d->image.isNull();
+}
+
+QImage VideoFrame::image()
+{
+	return d->image;
+}
+
+int VideoFrame::holdTime()
+{
+	return d->time;
+}
+
+QSize VideoFrame::size() const
+{
+	return d->image.size();
+}
+
+void VideoFrame::setImage(const QImage& frame)
+{
+	d->image = frame.copy();
+}
+
+void VideoFrame::setHoldTime(int holdTime)
+{
+	d->time = holdTime; 
+}*/
+
