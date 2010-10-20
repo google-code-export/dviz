baf0e6189a5f7c34cf139c471c4dd7759cef873c
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2010-10-20 15:28:33 +0000

    Seperated out the text rendering functionality from TextVideoSource into RichTextRenderer. Discovered another show/hide bug with the text layer crossfading code - not fixed yet.

--- trunk/src/glvidtex/TextVideoSource.cpp
+++ trunk/src/glvidtex/TextVideoSource.cpp
@@ -1,37 +1,20 @@
 #include "TextVideoSource.h"
 #include "../ImageFilters.h"
 
-QCache<QString,double> TextVideoSource::static_autoTextSizeCache;
-
 TextVideoSource::TextVideoSource(QObject *parent)
 	: VideoSource(parent)
 	, m_textWidth(640)
-	, m_outlineEnabled(true)
-	, m_outlinePen(Qt::black, 2.0)
-	, m_fillEnabled(true)
-	, m_fillBrush(Qt::white)
-	, m_shadowEnabled(true)
-	, m_shadowBlurRadius(16)
-	, m_shadowOffsetX(3)
-	, m_shadowOffsetY(3)
 	, m_frameChanged(false)
-	, m_updatesLocked(false)
 {
 	setImage(QImage());
 	setIsBuffered(false);
-// 	qDebug() << "TextVideoSource::ctor(): \t in thread:"<<QThread::currentThreadId();
-	connect(&m_updateTimer, SIGNAL(timeout()), this, SLOT(renderText()));
-	m_updateTimer.setInterval(100);
-	m_updateTimer.setSingleShot(true);
+	m_renderer = new RichTextRenderer(this);
+	connect(m_renderer, SIGNAL(textRendered(QImage)), this, SLOT(setImage(const QImage&)));
 }
 
 void TextVideoSource::setHtml(const QString& html)
 {
-	m_html = html;
-	// dont block calling thread by updating - since we are in a thread ourself, 
-	// this should put the update into our thread
-	//QTimer::singleShot(0,this,SLOT(update()));
-	update();
+	m_renderer->setHtml(html);
 }
 
 void TextVideoSource::setImage(const QImage& img)
@@ -44,9 +27,7 @@ void TextVideoSource::setImage(const QImage& img)
 
 bool TextVideoSource::lockUpdates(bool flag)
 {
-	bool oldValue = m_updatesLocked;
-	m_updatesLocked = flag;
-	return oldValue;
+	return m_renderer->lockUpdates(flag);
 }
 
 void TextVideoSource::run()
@@ -54,7 +35,7 @@ void TextVideoSource::run()
 	while(!m_killed)
 	{
 //		qDebug() << "TextVideoSource::run()";
-		if(!m_updatesLocked)
+		if(!m_renderer->updatesLocked())
 		{
 			if(m_frameChanged)
 			{
@@ -68,389 +49,13 @@ void TextVideoSource::run()
 	}
 }
 
-
-
-void TextVideoSource::changeFontSize(double size)
-{
-	QTextDocument doc;
-	if (Qt::mightBeRichText(html()))
-		doc.setHtml(html());
-	else
-		doc.setPlainText(html());
-
-	QTextCursor cursor(&doc);
-	cursor.select(QTextCursor::Document);
-
-	QTextCharFormat format;
-	format.setFontPointSize(size);
-	cursor.mergeCharFormat(format);
-	cursor.mergeBlockCharFormat(format);
-
-	setHtml(doc.toHtml());
-}
-
-
-double TextVideoSource::findFontSize()
-{
-	QTextDocument doc;
-	if (Qt::mightBeRichText(html()))
-		doc.setHtml(html());
-	else
-		doc.setPlainText(html());
-
-	QTextCursor cursor(&doc);
-	cursor.select(QTextCursor::Document);
-	QTextCharFormat format = cursor.charFormat();
-	return format.fontPointSize();
-}
-
-int TextVideoSource::fitToSize(const QSize& size, int minimumFontSize, int maximumFontSize)
-{
-	int width = size.width();
-	int height = size.height();
-	
-	const QString sizeKey = QString("%1:%2:%3:%4").arg(html()).arg(width).arg(height).arg(minimumFontSize);
-	
-	// for centering
-	qreal boxHeight = -1;
-		
-	double ptSize = -1;
-	if(static_autoTextSizeCache.contains(sizeKey))
-	{
-		ptSize = *(static_autoTextSizeCache[sizeKey]);
-		
-		//qDebug()<<"TextVideoSource::fitToSize(): size search: CACHE HIT: loaded size:"<<ptSize;
-		
-		// We go thru the much-more-verbose method of creating
-		// the document and setting the html, width, merge cursor,
-		// etc, just so we can get the document height after
-		// setting the font size inorder to use it to center the textbox.
-		// If we didnt nead the height, we could just use autoText->setFontSize()
-		
-		QTextDocument doc;
-		doc.setTextWidth(width);
-		if (Qt::mightBeRichText(html()))
-			doc.setHtml(html());
-		else
-			doc.setPlainText(html());
-
-			
-		QTextCursor cursor(&doc);
-		cursor.select(QTextCursor::Document);
-		
-		QTextCharFormat format;
-		format.setFontPointSize(ptSize);
-		cursor.mergeCharFormat(format);
-		
-		boxHeight = doc.documentLayout()->documentSize().height();
-		
-		setHtml(doc.toHtml());
-	}
-	else
-	{
-		double ptSize = minimumFontSize > 0 ? minimumFontSize : findFontSize();
-		double sizeInc = 1;	// how big of a jump to add to the ptSize each iteration
-		int count = 0;		// current loop iteration
-		int maxCount = 100; 	// max iterations of the search loop
-		bool done = false;
-		
-		double lastGoodSize = ptSize;
-		QString lastGoodHtml = html();
-		
-		QTextDocument doc;
-		
-		qreal heightTmp;
-		
-		doc.setTextWidth(width);
-		if (Qt::mightBeRichText(html()))
-			doc.setHtml(html());
-		else
-			doc.setPlainText(html());
-
-			
-		QTextCursor cursor(&doc);
-		cursor.select(QTextCursor::Document);
-		
-		QTextCharFormat format;
-			
-		while(!done && count++ < maxCount)
-		{
-			format.setFontPointSize(ptSize);
-			cursor.mergeCharFormat(format);
-			
-			heightTmp = doc.documentLayout()->documentSize().height();
-			
-			if(heightTmp < height &&
-			      ptSize < maximumFontSize)
-			{
-				lastGoodSize = ptSize;
-				//lastGoodHtml = html();
-				boxHeight = heightTmp;
-
-				sizeInc *= 1.1;
-// 				qDebug()<<"size search: "<<ptSize<<"pt was good, trying higher, inc:"<<sizeInc<<"pt";
-				ptSize += sizeInc;
-
-			}
-			else
-			{
-// 				qDebug()<<"fitToSize: size search: last good ptsize:"<<lastGoodSize<<", stopping search";
-				done = true;
-			}
-		}
-		
-		if(boxHeight < 0 && minimumFontSize <= 0) // didnt find a size
-		{
-			ptSize = 100;
-			
-			count = 0;
-			done = false;
-			sizeInc = 1;
-			
-			//qDebug()<<"TextVideoSource::fitToSize(): size search: going UP failed, now I'll try to go DOWN";
-			
-			while(!done && count++ < maxCount)
-			{
-				
-				format.setFontPointSize(ptSize);
-				cursor.mergeCharFormat(format);
-				
-				heightTmp = doc.documentLayout()->documentSize().height();
-				
-				if(heightTmp < height)
-				{
-					lastGoodSize = ptSize;
-					//lastGoodHtml = html();
-					boxHeight = heightTmp;
-	
-					sizeInc *= 1.1;
-					//qDebug()<<"size search: "<<ptSize<<"pt was good, trying higher, inc:"<<sizeInc<<"pt";
-					ptSize -= sizeInc;
-	
-				}
-				else
-				{
-					//qDebug()<<"SongSlideGroup::textToSlides(): size search: last good ptsize:"<<lastGoodSize<<", stopping search";
-					done = true;
-				}
-			}
-		}
-
-		format.setFontPointSize(lastGoodSize);
-		cursor.mergeCharFormat(format);
-		
-		setHtml(doc.toHtml());
-		
-		//qDebug()<<"TextVideoSource::fitToSize(): size search: caching ptsize:"<<lastGoodSize<<", count: "<<count<<"( minimum size was:"<<minimumFontSize<<")";
-		boxHeight = heightTmp;
-		//static_autoTextSizeCache[sizeKey] = lastGoodSize;
-		
-		// We are using a QCache instead of a plain QMap, so that requires a pointer value 
-		// Using QCache because the key for the cache could potentially become quite large if there are large amounts of HTML
-		// and I dont want to just keep accumlating html in the cache infinitely
-		static_autoTextSizeCache.insert(sizeKey, new double(lastGoodSize),1);
-	}
-	
-	return (int)boxHeight;
-	
-}
-
-QSize TextVideoSource::findNaturalSize(int atWidth)
-{
-	QTextDocument doc;
-	if(atWidth > 0)
-		doc.setTextWidth(atWidth);
-	if (Qt::mightBeRichText(html()))
-		doc.setHtml(html());
-	else
-		doc.setPlainText(html());
-	
-	QSize firstSize = doc.documentLayout()->documentSize().toSize();
-	QSize checkSize = firstSize;
-	
-// 	qDebug() << "TextVideoSource::findNaturalSize: atWidth:"<<atWidth<<", firstSize:"<<firstSize;
-	
-	#define RUNAWAY_LIMIT 500
-	
-	int counter = 0;
-	int deInc = 10;
-	while(checkSize.height() == firstSize.height() &&
-	      checkSize.height() > 0 &&
-	      counter < RUNAWAY_LIMIT)
-	{
-		int w = checkSize.width() - deInc;
-		doc.setTextWidth(w);
-		checkSize = doc.documentLayout()->documentSize().toSize();
-		
-// 		qDebug() << "TextVideoSource::findNaturalSize: w:"<<w<<", checkSize:"<<checkSize<<", counter:"<<counter;
-		counter ++;
-	}
-	
-	if(checkSize.width() != firstSize.width())
-	{
-		int w = checkSize.width() + deInc;
-		doc.setTextWidth(w);
-		checkSize = doc.documentLayout()->documentSize().toSize();
-// 		qDebug() << "TextVideoSource::findNaturalSize: Final Size: w:"<<w<<", checkSize:"<<checkSize;
-		return checkSize;
-	}
-	else
-	{
-// 		qDebug() << "TextVideoSource::findNaturalSize: No Change, firstSize:"<<checkSize;
-		return firstSize;
-	}
-}
-
 void TextVideoSource::update()
 {
- 	if(m_updateTimer.isActive())
- 		m_updateTimer.stop();
-// 	qDebug() << "TextVideoSource::update(): \t in thread:"<<QThread::currentThreadId();
-	m_updateTimer.start();
+	m_renderer->update();
 }
 
-void TextVideoSource::renderText()
+void TextVideoSource::setTextWidth(int w)
 {
-// 	qDebug()<<itemName()<<"TextBoxWarmingThread::run(): htmlCode:"<<htmlCode;
-	//qDebug() << "TextVideoSource::update(): HTML:"<<html();
-	//qDebug() << "TextVideoSource::update(): Update Start...";
-// 	qDebug() << "TextVideoSource::renderText(): \t in thread:"<<QThread::currentThreadId();
-	
-	QTextDocument doc;
-	QTextDocument shadowDoc;
-	
-	if (Qt::mightBeRichText(html()))
-	{
-		doc.setHtml(html());
-		shadowDoc.setHtml(html());
-	}
-	else
-	{
-		doc.setPlainText(html());
-		shadowDoc.setPlainText(html());
-	}
-	
-	
-	int textWidth = m_textWidth;
-
-	doc.setTextWidth(textWidth);
-	shadowDoc.setTextWidth(textWidth);
-	
-	// Apply outline pen to the html
-	QTextCursor cursor(&doc);
-	cursor.select(QTextCursor::Document);
-
-	QTextCharFormat format;
-
-	QPen p(Qt::NoPen);
-	if(outlineEnabled())
-	{
-		p = outlinePen();
-		p.setJoinStyle(Qt::MiterJoin);
-	}
-
-	format.setTextOutline(p);
-	format.setForeground(fillEnabled() ? fillBrush() : Qt::NoBrush); //Qt::white);
-
-	cursor.mergeCharFormat(format);
-	
-	// Setup the shadow text formatting if enabled
-	if(shadowEnabled())
-	{
-		if(shadowBlurRadius() <= 0.05)
-		{
-			QTextCursor cursor(&shadowDoc);
-			cursor.select(QTextCursor::Document);
-	
-			QTextCharFormat format;
-			format.setTextOutline(Qt::NoPen);
-			format.setForeground(shadowBrush());
-	
-			cursor.mergeCharFormat(format);
-		}
-	}
-	
-			
-	QSizeF shadowSize = shadowEnabled() ? QSizeF(shadowOffsetX(),shadowOffsetY()) : QSizeF(0,0);
-	QSizeF docSize = doc.size();
-	QSize sumSize = (docSize + shadowSize).toSize();
-	//qDebug() << "TextVideoSource::update(): textWidth: "<<textWidth<<", shadowSize:"<<shadowSize<<", docSize:"<<docSize<<", sumSize:"<<sumSize;
-	QImage cache(sumSize,QImage::Format_ARGB32); //_Premultiplied);
-	memset(cache.scanLine(0),0,cache.byteCount());
-	
-	QPainter textPainter(&cache);
-	//textPainter.fillRect(cache.rect(),Qt::transparent);
-	
-	QAbstractTextDocumentLayout::PaintContext pCtx;
-
-	if(shadowEnabled())
-	{
-		if(shadowBlurRadius() <= 0.05)
-		{
-			// render a "cheap" version of the shadow using the shadow text document
-			textPainter.save();
-
-			textPainter.translate(shadowOffsetX(),shadowOffsetY());
-			shadowDoc.documentLayout()->draw(&textPainter, pCtx);
-
-			textPainter.restore();
-		}
-		else
-		{
-			double radius = shadowBlurRadius();
-			
-			// create temporary pixmap to hold a copy of the text
-			QSizeF blurSize = ImageFilters::blurredSizeFor(doc.size(), (int)radius);
-			//qDebug() << "Blur size:"<<blurSize<<", doc:"<<doc.size()<<", radius:"<<radius;
-			QImage tmpImage(blurSize.toSize(),QImage::Format_ARGB32_Premultiplied);
-			memset(tmpImage.scanLine(0),0,tmpImage.byteCount());
-			
-			// render the text
-			QPainter tmpPainter(&tmpImage);
-			
-			tmpPainter.save();
-			tmpPainter.translate(radius, radius);
-			doc.documentLayout()->draw(&tmpPainter, pCtx);
-			tmpPainter.restore();
-			
-			// blacken the text by applying a color to the copy using a QPainter::CompositionMode_DestinationIn operation. 
-			// This produces a homogeneously-colored pixmap.
-			QRect rect = tmpImage.rect();
-			tmpPainter.setCompositionMode(QPainter::CompositionMode_SourceIn);
-			tmpPainter.fillRect(rect, shadowBrush().color());
-			tmpPainter.end();
-
-			// blur the colored text
-			ImageFilters::blurImage(tmpImage, (int)radius);
-			
-			// render the blurred text at an offset into the cache
-			textPainter.save();
-			textPainter.translate(shadowOffsetX() - radius,
-					      shadowOffsetY() - radius);
-			textPainter.drawImage(0, 0, tmpImage);
-			textPainter.restore();
-		}
-	}
-	
-	doc.documentLayout()->draw(&textPainter, pCtx);
-	
-	textPainter.end();
-	
-	setImage(cache);
-	
-	//qDebug() << "TextVideoSource::update(): Update Done.";
+	m_textWidth = w;
+	m_renderer->setTextWidth(w);
 }
-
-ITEM_PROPSET(TextVideoSource, TextWidth,	int,	textWidth);
-
-ITEM_PROPSET(TextVideoSource, OutlineEnabled,	bool,	outlineEnabled);
-ITEM_PROPSET(TextVideoSource, OutlinePen,	QPen,	outlinePen);
-
-ITEM_PROPSET(TextVideoSource, FillEnabled,	bool,	fillEnabled);
-ITEM_PROPSET(TextVideoSource, FillBrush,	QBrush,	fillBrush);
-
-ITEM_PROPSET(TextVideoSource, ShadowEnabled,	bool,	shadowEnabled);
-ITEM_PROPSET(TextVideoSource, ShadowBlurRadius,	double,	shadowBlurRadius);
-ITEM_PROPSET(TextVideoSource, ShadowOffsetX,	double,	shadowOffsetX);
-ITEM_PROPSET(TextVideoSource, ShadowOffsetY,	double,	shadowOffsetY);
-ITEM_PROPSET(TextVideoSource, ShadowBrush,	QBrush,	shadowBrush);
