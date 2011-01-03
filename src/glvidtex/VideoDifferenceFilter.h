487e43a03dbfd80d9a66a196d2d535917a85948c
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2011-01-03 19:59:35 +0000

    Heavily reworked VideoDifferenceFilter in an attempt to improve thresholding values and metrics. Achieved marginally satisfactory results in background replacement/subtraction - still a good deal of work to do before it is useful in consumer-level applications.

--- src/glvidtex/VideoDifferenceFilter.h
+++ src/glvidtex/VideoDifferenceFilter.h
@@ -11,6 +11,35 @@ public:
 	VideoDifferenceFilter(QObject *parent=0);
 	~VideoDifferenceFilter();
 	
+	bool frameAccumEnabled() { return m_frameAccumEnabled; }
+	int frameAccumNum() { return m_frameAccumNum; }
+	
+	int threshold() { return m_threshold; }
+	int minTreshold() { return m_minThreshold; }
+
+	enum FilterType { FrameToFrame, FrameToFirstFrame, FrameToRefImage };
+	FilterType filterType() { return m_filterType; }
+	
+	QImage referenceImage() { return m_refImage; }
+	
+	enum OutputType { BinarySmoothed, Binary, JustNewPixels, HighlightNewPixels, BackgroundReplace };
+	OutputType outputType() { return m_outputType; } 
+	
+public slots:
+	void setFrameAccumEnabled(bool);
+	void setFrameAccumNum(int);
+	
+	void setThreshold(int);
+	void setMinThreshold(int);
+
+	void setFilterType(FilterType);
+	void setReferenceImage(QImage);
+	
+	void setOutputType(OutputType);
+	
+signals:
+	void deltaAverages(int h, int s, int v, int metric);
+	
 protected:
 	virtual void processFrame();
 	
@@ -18,6 +47,24 @@ protected:
 	
 	QImage m_lastImage;
 	bool m_includeOriginalImage;
+
+	bool m_frameAccumEnabled;
+	int m_frameAccumNum;
+	
+	int m_threshold;
+	int m_minThreshold;
+	
+	FilterType m_filterType;
+	QImage m_refImage; 
+	QImage m_newBackground;
+	
+	OutputType m_outputType;
+	
+	QQueue<QImage> m_frameAccum;
+	
+	QQueue<QImage> m_firstFrameAccum;
+	
+	int m_frameCount;
 };
 
 #endif
