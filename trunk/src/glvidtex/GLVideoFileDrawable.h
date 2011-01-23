1d033eb4d8ef0ecc53e6e2bf4c72097977813a10
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2011-01-23 02:30:46 +0000

    Minor updates to GLVideoFileDrawable - nothing really finished there yet.

--- trunk/src/glvidtex/GLVideoFileDrawable.h
+++ trunk/src/glvidtex/GLVideoFileDrawable.h
@@ -10,21 +10,40 @@ class GLVideoFileDrawable : public GLVideoDrawable
 	
 	Q_PROPERTY(QString videoFile READ videoFile WRITE setVideoFile USER true);
 	
+	Q_PROPERTY(int volume READ volume WRITE setVolume);
+	Q_PROPERTY(bool muted READ isMuted WRITE setMuted);
+	Q_PROPERTY(int status READ status WRITE setStatus);
+	Q_PROPERTY(quint64 position READ position WRITE setPosition);
+	
 public:
 	GLVideoFileDrawable(QString file="", QObject *parent=0);
 	
 	QString videoFile() { return m_videoFile; }
+	
 	double videoLength();
+	int volume();
+	bool isMuted();
+	int status();
+	quint64 position();
 	
 signals:
 	void videoFileChanged(const QString&);
+	void positionChanged(qint64 position);
 	
 public slots:
 	bool setVideoFile(const QString&);
 	
+	void setVolume(int);
+	void setMuted(bool);
+	void setStatus(int);
+	void setPosition(qint64);
+	
 private slots:
 	void testXfade();
 	void deleteSource(VideoSource *source);
+	
+protected:
+	virtual QVariant itemChange(GraphicsItemChange change, const QVariant &value);
 	
 private:
 	QString m_videoFile;
