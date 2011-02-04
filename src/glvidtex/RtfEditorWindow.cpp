4c0b611a5ae2ab0dd5753d7a48efb5546496dcf9
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2011-02-04 16:10:41 +0000

    Added controls to enable/disable scroller mode to RtfEditorWindow (used in DirectorWindow and EditorWindow).

--- src/glvidtex/RtfEditorWindow.cpp
+++ src/glvidtex/RtfEditorWindow.cpp
@@ -17,7 +17,7 @@ RtfEditorWindow::RtfEditorWindow(GLTextDrawable *gld, QWidget *parent)
 
 	opts.reset();
 	
-	QHBoxLayout *countdown = new QHBoxLayout();
+	QHBoxLayout *controls1 = new QHBoxLayout();
 	QWidget *boolEdit = PropertyEditorFactory::generatePropertyEditor(gld, "isCountdown", SLOT(setIsCountdown(bool)), opts);
 	QWidget *dateEdit  = PropertyEditorFactory::generatePropertyEditor(gld, "targetDateTime", SLOT(setTargetDateTime(QDateTime)), opts);
 	QCheckBox *box = dynamic_cast<QCheckBox*>(boolEdit);
@@ -26,8 +26,8 @@ RtfEditorWindow::RtfEditorWindow(GLTextDrawable *gld, QWidget *parent)
 		connect(boolEdit, SIGNAL(toggled(bool)), dateEdit, SLOT(setEnabled(bool)));
 		dateEdit->setEnabled(box->isChecked());
 	}
-	countdown->addWidget(boolEdit);
-	countdown->addWidget(dateEdit);
+	controls1->addWidget(boolEdit);
+	controls1->addWidget(dateEdit);
 	
 	QWidget *boolEdit2 = PropertyEditorFactory::generatePropertyEditor(gld, "isClock", SLOT(setIsClock(bool)), opts);
 	QWidget *stringEdit = PropertyEditorFactory::generatePropertyEditor(gld, "clockFormat", SLOT(setClockFormat(const QString&)), opts);
@@ -38,11 +38,55 @@ RtfEditorWindow::RtfEditorWindow(GLTextDrawable *gld, QWidget *parent)
 		stringEdit->setEnabled(box->isChecked());
 	}
 		
-	countdown->addWidget(boolEdit2);
-	countdown->addWidget(stringEdit);
+	controls1->addWidget(boolEdit2);
+	controls1->addWidget(stringEdit);
 	
-	countdown->addStretch(1);
-	layout->addLayout(countdown);
+	controls1->addStretch(1);
+	layout->addLayout(controls1);
+	
+	QFrame *frame1 = new QFrame(this);
+	frame1->setFrameStyle(QFrame::HLine | QFrame::Sunken);
+	frame1->setMidLineWidth(1);
+	layout->addWidget(frame1);
+	
+	
+	QHBoxLayout *controls2 = new QHBoxLayout();
+	
+	QWidget *boolEdit3 = PropertyEditorFactory::generatePropertyEditor(gld, "isScroller", SLOT(setIsScroller(bool)), opts);
+	
+	opts.reset();
+	opts.type = QVariant::Int;
+	opts.min = 1;
+	opts.max = 100;
+	opts.suffix = " px";
+	opts.defaultValue = 10;
+	QWidget *spinEdit = PropertyEditorFactory::generatePropertyEditor(gld, "scrollerSpeed", SLOT(setScrollerSpeed(int)), opts);
+	
+	opts.reset();
+	opts.stringIsFile = true;
+	QWidget *fileEdit  = PropertyEditorFactory::generatePropertyEditor(gld, "iconFile", SLOT(setIconFile(const QString&)), opts);
+	
+	box = dynamic_cast<QCheckBox*>(boolEdit3);
+	if(box)
+	{
+		connect(boolEdit3, SIGNAL(toggled(bool)), spinEdit, SLOT(setEnabled(bool)));
+		connect(boolEdit3, SIGNAL(toggled(bool)), fileEdit, SLOT(setEnabled(bool)));
+		spinEdit->setEnabled(box->isChecked());
+		fileEdit->setEnabled(box->isChecked());
+	}
+		
+	controls2->addWidget(boolEdit3);
+	controls2->addWidget(spinEdit);
+	controls2->addWidget(fileEdit);
+	
+	controls2->addStretch(1);
+	layout->addLayout(controls2);
+	
+	QFrame *frame2 = new QFrame(this);
+	frame2->setFrameStyle(QFrame::HLine | QFrame::Sunken);
+	frame2->setMidLineWidth(1);
+	layout->addWidget(frame2);
+	
 	
 	m_rtfEditor = new RichTextEditorDialog();
 	m_rtfEditor->setText(gld->text());
