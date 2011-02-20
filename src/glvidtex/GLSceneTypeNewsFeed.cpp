77f73c896cea6bea43852068d9089ee5a6978125
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2011-02-20 02:24:03 +0000

    Added a generic RSS parser class to GLSceneTypeNewsItem for futher testing.

--- src/glvidtex/GLSceneTypeNewsFeed.cpp
+++ src/glvidtex/GLSceneTypeNewsFeed.cpp
@@ -57,6 +57,9 @@ GLSceneTypeNewsFeed::GLSceneTypeNewsFeed(QObject *parent)
 	setParam("updateTime", 15);
 	
 	reloadData();
+	
+	RssParser *parser = new RssParser("http://www.mypleasanthillchurch.org/phc/boards/rss", this);
+	connect(parser, SIGNAL(itemsAvailable(QList<RssParser::RssItem>)), this, SLOT(itemsAvailable(QList<RssParser::RssItem>)));
 }
 
 void GLSceneTypeNewsFeed::setLiveStatus(bool flag)
@@ -131,8 +134,9 @@ void GLSceneTypeNewsFeed::reloadData()
 	requestData("");//location());
 }
 
-void GLSceneTypeNewsFeed::requestData(const QString &location) 
+void GLSceneTypeNewsFeed::requestData(const QString &/*location*/) 
 {
+/*
 	QUrl url("http://www.google.com/ig/api?news");
 // 	url.addEncodedQueryItem("hl", "en");
 // 	url.addEncodedQueryItem("weather", QUrl::toPercentEncoding(location));
@@ -143,6 +147,7 @@ void GLSceneTypeNewsFeed::requestData(const QString &location)
 	connect(manager, SIGNAL(finished(QNetworkReply*)),
 		this, SLOT(handleNetworkData(QNetworkReply*)));
 	manager->get(QNetworkRequest(url));
+*/
 }
 
 void GLSceneTypeNewsFeed::handleNetworkData(QNetworkReply *networkReply) 
@@ -210,4 +215,137 @@ void GLSceneTypeNewsFeed::parseData(const QString &data)
 	
 	if(scene())
 		showNextItem();
+}
+
+void GLSceneTypeNewsFeed::itemsAvailable(QList<RssParser::RssItem> list)
+{
+	m_news = list;
+	m_currentIndex = 0;
+	if(scene())
+		showNextItem();
+}
+
+/////////////////////////////////////////////
+
+RssParser::RssParser(QString url, QObject *parent)
+	: QObject(parent)
+{
+	if(!url.isEmpty())
+		setUrl(url);
+
+	connect(&m_reloadTimer, SIGNAL(timeout()), this, SLOT(reloadData()));
+	setUpdateTime(60);
+}
+
+void RssParser::setUpdateTime(int min)
+{
+	m_reloadTimer.setInterval(min * 60 * 1000);
+}
+
+void RssParser::reloadData()
+{
+	loadUrl(m_url);
+}
+
+void RssParser::setUrl(const QString& url)
+{
+	m_url = url;
+	reloadData();
+}
+
+
+void RssParser::loadUrl(const QString &location) 
+{
+	QUrl url(location);
+// 	url.addEncodedQueryItem("hl", "en");
+// 	url.addEncodedQueryItem("weather", QUrl::toPercentEncoding(location));
+	
+	qDebug() << "RssParser::loadUrl(): url:"<<url;
+
+	QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+	connect(manager, SIGNAL(finished(QNetworkReply*)),
+		this, SLOT(handleNetworkData(QNetworkReply*)));
+	manager->get(QNetworkRequest(url));
+}
+
+void RssParser::handleNetworkData(QNetworkReply *networkReply) 
+{
+	QUrl url = networkReply->url();
+	if (!networkReply->error())
+		parseData(QString::fromUtf8(networkReply->readAll()));
+	networkReply->deleteLater();
+	networkReply->manager()->deleteLater();
+}
+/*
+#define GET_DATA_ATTR xml.attributes().value("data").toString() \
+	.replace("&amp;#39;","'") \
+	.replace("&amp;quot;","\"") \
+	.replace("&amp;amp;","&")
+	*/
+
+void RssParser::parseData(const QString &data) 
+{
+	qDebug() << "RssParser::parseData()";//: "<<data;
+	m_items.clear();
+	
+	QXmlStreamReader xml(data);
+	while (!xml.atEnd()) 
+	{
+		xml.readNext();
+		if (xml.tokenType() == QXmlStreamReader::StartElement) 
+		{
+// 			qDebug() << "RssParser::parseData(): StartElement/xml.name():"<<xml.name();
+			// Parse and collect the news items
+			if (xml.name() == "item") 
+			{
+				QString tag;
+				RssItem item;
+				while (!xml.atEnd()) 
+				{
+					xml.readNext();
+					if (xml.name() == "item") 
+					{
+						if (!item.title.isEmpty()) 
+						{
+							item.date = item.date.replace("T"," ");
+							item.date = item.date.replace("-05:00","");
+						
+							m_items << item;
+							qDebug() << "RssParser::parseData(): Added item: title:"<<item.title
+								<<", "<<item.url
+								<<", "<<item.date
+								<<", "<<item.source; 
+							
+							item = RssItem();
+						} 
+						break;
+					}
+					
+					if (xml.tokenType() == QXmlStreamReader::StartElement) 
+					{
+						tag = xml.name().toString();
+// 						qDebug() << "RssParser::parseData(): 	StartElement/xml.name():"<<xml.name();
+					}
+					else 
+					if (xml.isCharacters() && !xml.isWhitespace()) 
+					{
+// 						if(tag != "description")
+// 							qDebug() << "RssParser::parseData(): 		isCharacters/tag:"<<tag<<", text:"<<xml.text().toString();
+						if (tag == "title")
+							item.title += xml.text().toString();
+						if (tag == "link") 
+							item.url += xml.text().toString();
+						if (tag == "creator") 
+							item.source += xml.text().toString();;
+						if (tag == "date")
+							item.date += xml.text().toString();;
+						if (tag == "description")
+							item.text += xml.text().toString();;
+					}
+				}
+			}
+		}
+	}
+	
+	emit itemsAvailable(m_items);
 }
--- src/glvidtex/GLSceneTypeNewsFeed.h
+++ src/glvidtex/GLSceneTypeNewsFeed.h
@@ -35,6 +35,60 @@
 */			
 
 
+class RssParser : public QObject
+{
+	Q_OBJECT
+	
+	Q_PROPERTY(QString url READ url WRITE setUrl);
+	Q_PROPERTY(int updateTime READ updateTime WRITE setUpdateTime);
+	
+public:
+	RssParser(QString url="", QObject *parent=0);
+	
+	class RssItem
+	{
+	public:
+		QString title;
+		QString text;
+		QString source;
+		QString url;
+		QString date;
+	};
+	
+	QString url() { return m_url; }
+	int updateTime() { return m_updateTime; }
+
+	QList<RssItem> items() { return m_items; }
+
+signals:
+	void itemsAvailable(QList<RssParser::RssItem>);
+
+	
+public slots:
+	/** Set the time to wait between updates to \a minutes */
+	void setUpdateTime(int minutes);
+		
+	/** Reload the data from server*/
+	void reloadData();
+	
+	void setUrl(const QString&);
+
+private slots:
+	void loadUrl(const QString &url);
+	void handleNetworkData(QNetworkReply *networkReply);
+	void parseData(const QString &data);
+
+private:
+	QTimer m_reloadTimer;
+		
+	QString m_url;
+	int m_updateTime;
+	QList<RssItem> m_items;
+
+};
+
+
+
 class GLSceneTypeNewsFeed : public GLSceneType
 {
 	Q_OBJECT
@@ -71,19 +125,22 @@ private slots:
 	void parseData(const QString &data);
 	
 	void showNextItem();
+	
+	void itemsAvailable(QList<RssParser::RssItem>);
 
 private:
 	QTimer m_reloadTimer;
 	
-	class NewsItem
-	{
-	public:
-		QString title;
-		QString text;
-		QString source;
-		QString url;
-		QString date;
-	};
+	typedef RssParser::RssItem NewsItem;
+// 	class NewsItem
+// 	{
+// 	public:
+// 		QString title;
+// 		QString text;
+// 		QString source;
+// 		QString url;
+// 		QString date;
+// 	};
 	
 	QList<NewsItem> m_news;
 	int m_currentIndex;
