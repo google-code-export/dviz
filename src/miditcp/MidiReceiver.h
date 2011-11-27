35a3ccc990b423d97af7590a01d1e8bf1251643c
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2011-11-27 20:36:02 +0000

    Added MidiReceiver and main_rxtest.cpp to the miditcp folder to test the receiving side of the MIDI tcp server

---
+++ src/miditcp/MidiReceiver.h
@@ -0,0 +1,50 @@
+#ifndef MidiReceiver_H
+#define MidiReceiver_H
+
+#include <QDialog>
+#include <QTcpSocket>
+
+class QTcpSocket;
+
+class MidiReceiver : public QObject
+{
+	Q_OBJECT
+
+public:
+	MidiReceiver(bool verbose=false, QObject *parent = 0);
+	~MidiReceiver();
+	
+	bool start(const QString& host="", int port=3729);
+	void exit();
+
+signals:
+	void midiFrameReady(int chan, int key, int val);
+	
+	void socketDisconnected();
+	void socketError(QAbstractSocket::SocketError);
+	void error(QString);	
+	void socketConnected();
+	void connected();
+
+private slots:
+	void dataReady();
+	void processBlock();
+	void lostConnection();
+	void lostConnection(QAbstractSocket::SocketError);
+	void reconnect();
+	void connectionReady();
+
+	void emitError(QAbstractSocket::SocketError socketError);
+
+private:
+	QTcpSocket *m_socket;
+	QByteArray m_dataBlock;
+	bool m_connected;
+	QString m_host;
+	int m_port;
+	bool m_verbose;
+	bool m_autoReconnect;
+
+};
+
+#endif
