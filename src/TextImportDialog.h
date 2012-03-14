7cea308ffa52c1b3540ae14ebb847a6f608c8fbc
josiahbryan <josiahbryan@5ee4d49e-9b41-11de-8595-bbd473b9459f>
2012-03-14 21:12:03 +0000

    Started work on the TextImportDialog

--- src/TextImportDialog.h
+++ src/TextImportDialog.h
@@ -7,17 +7,31 @@ namespace Ui {
     class TextImportDialog;
 }
 
-class TextImportDialog : public QDialog {
-    Q_OBJECT
-public:
-    TextImportDialog(QWidget *parent = 0);
-    ~TextImportDialog();
+class SlideGroup;
 
+class TextImportDialog : public QDialog 
+{
+	Q_OBJECT
+public:
+	TextImportDialog(QWidget *parent = 0);
+	~TextImportDialog();
+
+
+protected slots:
+	void templateChanged(SlideGroup*);
+	
+	void doImport();
+	void browseBtn();
+	
 protected:
-    void changeEvent(QEvent *e);
+	void changeEvent(QEvent *e);
+	
+	void setupUi();
 
 private:
-    Ui::TextImportDialog *m_ui;
+	Ui::TextImportDialog *m_ui;
+	
+	SlideGroup *m_template;
 };
 
 #endif // TEXTIMPORTDIALOG_H
